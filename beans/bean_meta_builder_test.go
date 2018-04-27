dfe9f1e5deb015dc1fdb992f9a23ff80c3908398
Aren Chen <yarencheng@gmail.com>
2018-04-27 08:26:17 -0700

    refactor

--- beans/bean_meta_builder_test.go
+++ beans/bean_meta_builder_test.go
@@ -1,21 +1,51 @@
 package beans
 
 import (
-	"reflect"
 	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
+
+// MockPropertyMetaData is a mock of PropertyMetaData interface
+type MockPropertyMetaData struct {
+	mock.Mock
+}
+
+func (m *MockPropertyMetaData) GetName() string {
+	args := m.Called()
+	return args.String(0)
+}
+
+func (m *MockPropertyMetaData) GetReference() string {
+	args := m.Called()
+	return args.String(0)
+}
+
+func (m *MockPropertyMetaData) GetBean() BeanMetaData {
+	args := m.Called()
+	return args.Get(0).(BeanMetaData)
+}
+
+func (m *MockPropertyMetaData) IsReference() bool {
+	args := m.Called()
+	return args.Bool(0)
+}
 
 func Test_bean_GetProperties(t *testing.T) {
 
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
 	expect := make([]PropertyMetaData, 1)
-	expect[0] = &propertyBean{}
+	expect[0] = &MockPropertyMetaData{}
 
-	type A struct{ i int }
+	bean := bean{
+		properties: expect,
+	}
 
-	expect[0] = &propertyBean{}
-	// expect[0] = &A{}
+	actual := bean.GetProperties()
 
-	reflect.TypeOf("")
-
-	//vean := bean{]}
+	assert.ElementsMatch(t, expect, actual)
 }
