3a4a37824d074899ae5f644581b69dafec3e2e63
Aren Chen <yarencheng@gmail.com>
2018-04-28 15:44:30 -0700

    add more unit test

--- config2/application_context_test.go
+++ config2/application_context_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_applicationContext_GetBean_singleton(t *testing.T) {
+func Test_applicationContext_GetBean_defaultScope_string(t *testing.T) {
 
 	// arrange
 	ctx, _ := ApplicationContext(Beans(
@@ -34,7 +34,7 @@ func Test_applicationContext_GetBean_singleton(t *testing.T) {
 	assert.Equal(t, unsafe.Pointer(s1), unsafe.Pointer(s2))
 }
 
-func Test_applicationContext_GetBean_singletonsss(t *testing.T) {
+func Test_applicationContext_GetBean_defaultScope_struct(t *testing.T) {
 
 	// arrange
 	type beanStruct struct{ s string }
@@ -60,4 +60,113 @@ func Test_applicationContext_GetBean_singletonsss(t *testing.T) {
 	assert.True(t, ok2)
 
 	assert.Equal(t, unsafe.Pointer(s1), unsafe.Pointer(s2))
+}
+func Test_applicationContext_GetBean_singleton_string(t *testing.T) {
+
+	// arrange
+	ctx, _ := ApplicationContext(Beans(
+		Bean("").Id("id").Singleton(),
+	))
+
+	// action
+	bean1, e1 := ctx.GetBean("id")
+	if e1 != nil {
+		t.FailNow()
+	}
+	bean2, e2 := ctx.GetBean("id")
+	if e2 != nil {
+		t.FailNow()
+	}
+
+	// aasert
+
+	s1, ok1 := bean1.(*string)
+	assert.True(t, ok1)
+	s2, ok2 := bean2.(*string)
+	assert.True(t, ok2)
+
+	assert.Equal(t, unsafe.Pointer(s1), unsafe.Pointer(s2))
+}
+
+func Test_applicationContext_GetBean_singleton_struct(t *testing.T) {
+
+	// arrange
+	type beanStruct struct{ s string }
+	ctx, _ := ApplicationContext(Beans(
+		Bean(beanStruct{}).Id("id").Singleton(),
+	))
+
+	// action
+	bean1, e1 := ctx.GetBean("id")
+	if e1 != nil {
+		t.FailNow()
+	}
+	bean2, e2 := ctx.GetBean("id")
+	if e2 != nil {
+		t.FailNow()
+	}
+
+	// aasert
+
+	s1, ok1 := bean1.(*beanStruct)
+	assert.True(t, ok1)
+	s2, ok2 := bean2.(*beanStruct)
+	assert.True(t, ok2)
+
+	assert.Equal(t, unsafe.Pointer(s1), unsafe.Pointer(s2))
+}
+
+func Test_applicationContext_GetBean_prototype_string(t *testing.T) {
+
+	// arrange
+	ctx, _ := ApplicationContext(Beans(
+		Bean("").Id("id").Prototype(),
+	))
+
+	// action
+	bean1, e1 := ctx.GetBean("id")
+	if e1 != nil {
+		t.FailNow()
+	}
+	bean2, e2 := ctx.GetBean("id")
+	if e2 != nil {
+		t.FailNow()
+	}
+
+	// aasert
+
+	s1, ok1 := bean1.(*string)
+	assert.True(t, ok1)
+	s2, ok2 := bean2.(*string)
+	assert.True(t, ok2)
+
+	assert.NotEqual(t, unsafe.Pointer(s1), unsafe.Pointer(s2))
+}
+
+func Test_applicationContext_GetBean_prototype_struct(t *testing.T) {
+
+	// arrange
+	type beanStruct struct{ s string }
+	ctx, _ := ApplicationContext(Beans(
+		Bean(beanStruct{}).Id("id").Prototype(),
+	))
+
+	// action
+	bean1, e1 := ctx.GetBean("id")
+	if e1 != nil {
+		t.FailNow()
+	}
+	bean2, e2 := ctx.GetBean("id")
+	if e2 != nil {
+		t.FailNow()
+	}
+
+	// aasert
+
+	s1, ok1 := bean1.(*beanStruct)
+	assert.True(t, ok1)
+	s2, ok2 := bean2.(*beanStruct)
+	assert.True(t, ok2)
+
+	assert.NotEqual(t, unsafe.Pointer(s1), unsafe.Pointer(s2))
 }
