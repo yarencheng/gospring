b57603a5f0b5c3d926e78ac01e5406bfd121b03a
Aren Chen <yarencheng@gmail.com>
2018-05-04 00:23:30 +0800

    add builder function for []Bean

--- refactor/builder.go
+++ refactor/builder.go
@@ -18,3 +18,30 @@ func Bean(tvpe interface{}) StructBeanI {
 		finalize:   &f,
 	}
 }
+
+func Beans(values ...interface{}) []BeanI {
+
+	beans := make([]BeanI, len(values))
+
+	for i, value := range values {
+		switch value.(type) {
+		case StructBeanI:
+			beans[i] = value.(BeanI)
+			continue
+		case ReferenceBeanI:
+			beans[i] = value.(BeanI)
+			continue
+		case ValueBeanI:
+			beans[i] = value.(BeanI)
+			continue
+		default:
+		}
+
+		bean := &valueBean{
+			value: value,
+		}
+		beans[i] = bean
+	}
+
+	return beans
+}
