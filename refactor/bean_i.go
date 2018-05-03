a01d3efe1d138a4ad8077fd8f4dcfae514451a3c
Aren Chen <yarencheng@gmail.com>
2018-05-03 22:54:16 +0800

    create interface of a bean

--- refactor/bean_i.go
+++ refactor/bean_i.go
@@ -1,3 +1,14 @@
 package refactor
 
-type BeanI interface{}
+type BeanI interface {
+	Factory(fn interface{}, argv ...interface{}) BeanI
+	Finalize(fnName string) BeanI
+	Id(id string) BeanI
+	Init(fnName string) BeanI
+	Property(name string, values ...interface{}) BeanI
+	GetFactory() interface{}
+	GetFactoryArgv() []interface{}
+	GetId() *string
+	GetInit() *string
+	GetProperty(name string) []interface{}
+}
