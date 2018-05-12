3af520f98c04b1c991300b2fc66a4f4f2b796a3d
Aren Chen <yarencheng@gmail.com>
2018-05-12 18:50:56 +0800

    add unit test

--- application_context_test.go
+++ application_context_test.go
@@ -951,3 +951,25 @@ func Test_createBeanByFactory_injectArgvAsElem(t *testing.T) {
 	assert.NotNil(t, bean)
 	assert.Nil(t, e)
 }
+
+func Test_createBeanByFactory_convertArgvFailed(t *testing.T) {
+	// arrange
+	type beanStruct struct {
+		I interface{}
+	}
+	beans := Beans(
+		Bean(beanStruct{}).ID("1").Factory(
+			func(int) *beanStruct { return &beanStruct{} },
+			Bean(beanStruct{}),
+		),
+	)
+	ctx, e := NewApplicationContext(beans...)
+	require.Nil(t, e)
+
+	// action
+	bean, e := ctx.GetBean("1")
+
+	// assert
+	assert.Nil(t, bean)
+	assert.NotNil(t, e)
+}
