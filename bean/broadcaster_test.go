65ed2b1a2ab9bbecd5dea636a9933ee97275cbf4
Aren Chen <yarencheng@gmail.com>
2018-07-02 06:02:46 -0700

    test broadcast with struct

--- bean/broadcaster_test.go
+++ bean/broadcaster_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/yarencheng/gospring/v1"
 )
 
-func Test_Broadcaster_GetValue(t *testing.T) {
+func Test_Broadcaster_GetValue_intChannel(t *testing.T) {
 	// arrange: mock input source
 	sourceCh := make(chan int)
 	sourceBean := new(mocks.BeanMock)
@@ -47,4 +47,46 @@ func Test_Broadcaster_GetValue(t *testing.T) {
 	i2, ok := value2.Recv()
 	assert.True(t, ok)
 	assert.Equal(t, 123, i2.Interface().(int))
+}
+
+func Test_Broadcaster_GetValue_structChannel(t *testing.T) {
+	type TestStruct struct {
+		I int
+	}
+
+	// arrange: mock input source
+	sourceCh := make(chan TestStruct)
+	sourceBean := new(mocks.BeanMock)
+	sourceBean.On("GetValue").Return(func() reflect.Value {
+		return reflect.ValueOf(sourceCh)
+	}(), nil)
+
+	ctx := new(mocks.ApplicationContextMock)
+	ctx.On("GetBeanByID", "from_id").Return(sourceBean, true)
+
+	// arrange: create broad cast bean
+	bean, err := V1BroadcastParser(ctx, &v1.Broadcast{
+		SourceID: "from_id",
+		Size:     1,
+	})
+	require.NotNil(t, bean)
+	require.NoError(t, err)
+
+	// action: get 2 output channel
+	value1, err := bean.GetValue()
+	require.True(t, value1.IsValid())
+	require.NoError(t, err)
+	value2, err := bean.GetValue()
+	require.True(t, value2.IsValid())
+	require.NoError(t, err)
+	// action: send data to input channel
+	sourceCh <- TestStruct{I: 123}
+
+	// assert: both channel receive same data
+	i1, ok := value1.Recv()
+	assert.True(t, ok)
+	assert.Equal(t, TestStruct{I: 123}, i1.Interface().(TestStruct))
+	i2, ok := value2.Recv()
+	assert.True(t, ok)
+	assert.Equal(t, TestStruct{I: 123}, i2.Interface().(TestStruct))
 }
